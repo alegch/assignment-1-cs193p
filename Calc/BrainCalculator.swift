cb0befd0e57fd2024cffe7d494973c3f649b8c3f
alegch <alegch@bk.ru>
2016-05-05 12:19:11 +0400

    history label

--- Calc/BrainCalculator.swift
+++ Calc/BrainCalculator.swift
@@ -7,3 +7,119 @@
 //
 
 import Foundation
+
+class BrainCalculator {
+    let operations:Dictionary<String, Operation> = ["+": Operation.Binary({$0 + $1}),
+                                                    "−": Operation.Binary({$0 - $1}),
+                                                    "×": Operation.Binary({$0 * $1}),
+                                                    "÷": Operation.Binary({$0 / $1}),
+                                                   "Pi": Operation.Constant(M_PI),
+                                                  "sin": Operation.Unary({sin($0)}),
+                                                  "cos": Operation.Unary({cos($0)}),
+                                                   "tg": Operation.Unary({tan($0)}),
+                                                    "∓": Operation.Unary({-$0}),
+                                                    "=": Operation.Equality]
+    var accamulator = 0.0
+    var binaryTemp:BinaryTemp? = nil
+    internal private(set) var log = ""
+    internal private(set) var isPartialResult = false
+    var lastOperand = 0.0
+    var endComputing = false
+    var isWaitBinaryOperand = true
+    var isNeedReset = false
+    
+    func reset() -> Void {
+        accamulator = 0.0
+        log = ""
+        isPartialResult = false
+        lastOperand = 0.0
+        endComputing = false
+        isWaitBinaryOperand = true
+        isNeedReset = false
+    }
+    
+    func setOperand(operand: Double) {
+        setOperand(operand, functionName: "", logValue: true)
+    }
+    
+    func setOperand(operand: Double, functionName:String, logValue:Bool) {
+        accamulator = operand
+        if isNeedReset {
+            log = logValue ? String(operand) : functionName
+            isNeedReset = false
+        } else {
+            log += logValue ? String(operand) : functionName
+        }
+        
+        if isPartialResult {
+            isWaitBinaryOperand = true
+        } else {
+            isWaitBinaryOperand = false
+        }
+        lastOperand = operand
+    }
+    
+    
+    func result() -> Double {
+        return accamulator
+    }
+    
+    func performOperation(operation: String) -> Void {
+        let op = operations[operation]
+        if let function = op {
+            switch function {
+            case .Binary(let binaryFunction):
+                performIfExistBinary()
+                binaryTemp = BinaryTemp(firstArgument: accamulator, operation: binaryFunction)
+                isPartialResult = true
+                log += operation
+                if endComputing {
+                    isNeedReset = false
+                }
+            case .Equality:
+                if (!isWaitBinaryOperand) {
+                    log += String(lastOperand)
+                }
+                performIfExistBinary()
+                endComputing = true
+            case .Unary(let unaryFunction):
+                accamulator = unaryFunction(accamulator)
+                if endComputing {
+                    isNeedReset = true
+                }
+                
+                if binaryTemp != nil {
+                    let range = log.rangeOfString(String(lastOperand), options: NSStringCompareOptions.BackwardsSearch, range: nil, locale: nil);
+                    if let operandRange = range {
+                        let newOperand = operation + "(" + String(lastOperand) + ")"
+                        log = log.stringByReplacingCharactersInRange(operandRange, withString: newOperand)
+                    }
+                } else {
+                    log = operation + "(" + log + ")"
+                }
+            case .Constant(let constant):
+                setOperand(constant, functionName: operation, logValue: false)
+            }
+        }
+    }
+    
+    func performIfExistBinary() {
+        if binaryTemp != nil {
+            accamulator = binaryTemp!.operation(binaryTemp!.firstArgument, accamulator)
+            isPartialResult = false
+            binaryTemp = nil
+        }
+    }
+    
+    struct BinaryTemp {
+        var firstArgument:Double
+        var operation: (Double, Double) -> Double
+    }
+    
+    enum Operation {
+        case Unary((Double) -> Double)
+        case Constant(Double)
+        case Binary((Double, Double) -> Double)
+        case Equality
+    }
+}
--- Calc/ViewController.swift
+++ Calc/ViewController.swift
@@ -9,17 +9,66 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var displayLabel: UILabel!
+    @IBOutlet weak var historyLabel: UILabel!
+    
+    let brain = BrainCalculator()
+    var isTyping = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    var displayValue: Double {
+        set {
+            displayLabel.text = String(newValue)
+        }
+        get {
+            return Double(displayLabel.text!)!
+        }
     }
-
-
+    
+    var historyValue: String {
+        set {
+            historyLabel.text = newValue
+        }
+        get {
+            return historyLabel.text!
+        }
+    }
+    
+    @IBAction func onButtonTapped(sender: UIButton) {
+        let digit = sender.currentTitle!
+        if isTyping {
+            displayLabel.text = displayLabel.text! + digit
+        } else {
+            displayLabel.text = digit
+        }
+        
+        isTyping = true
+    }
+    
+    @IBAction func onOperationTapped(sender: UIButton) {
+        if (isTyping) {
+            brain.setOperand(displayValue)
+        }
+        isTyping = false
+        if let operation = sender.currentTitle {
+            brain.performOperation(operation)
+            displayValue = brain.result()
+            if brain.isPartialResult {
+                historyValue = brain.log + "..."
+            } else {
+                historyValue = brain.log + "="
+            }
+        }
+    }
+    
+    @IBAction func onResetTapped(sender: UIButton) {
+        brain.reset()
+        historyValue = brain.log
+        displayValue = brain.result()
+        isTyping = false
+    }
 }
 
